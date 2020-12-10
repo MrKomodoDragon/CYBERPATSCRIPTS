2112a1fe68ed7ab3e4ab93af22676c9ae2d51573
Saravanavasan <vasan@Ramadasss-MacBook-Air.local>
2020-12-09 17:55:08 -0800

    ""

--- users.sh
+++ users.sh
@@ -1,6 +1,13 @@
 # First update the authorized_users.txt file with the authorized users
 
 
-eval getent passwd {$(awk '/^UID_MIN/ {print $2}' /etc/login.defs)..$(awk '/^UID_MAX/ {print $2}' /etc/login.defs)} | cut -d: -f1 >> users.test 
+# eval getent passwd {$(awk '/^UID_MIN/ {print $2}' /etc/login.defs)..$(awk '/^UID_MAX/ {print $2}' /etc/login.defs)} | cut -d: -f1 >> users.txt
 cat authorized_users.txt users.txt >> merged.txt
-cat merged.txt | sort | uniq -u
+cat merged.txt | sort | uniq -u >> peeps_to_remove.txt
+
+cat peeps_to_remove.txt
+
+#cat peeps_to_remove.txt | while read line
+#do
+#    sudo userdel -r $line
+#done
