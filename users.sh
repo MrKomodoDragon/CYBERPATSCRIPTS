a496a60dcc05c1494850281f825107272b9dd365
Saravanavasan <vasan@Ramadasss-MacBook-Air.local>
2020-12-09 18:23:06 -0800

    ji

--- users.sh
+++ users.sh
@@ -2,8 +2,9 @@
 
 
 # eval getent passwd {$(awk '/^UID_MIN/ {print $2}' /etc/login.defs)..$(awk '/^UID_MAX/ {print $2}' /etc/login.defs)} | cut -d: -f1 >> users.txt
-cat authorized_users.txt users.txt >> merged.txt
-cat merged.txt | sort | uniq -u >> peeps_to_remove.txt
+ls /Users > users.txt
+cat authorized_users.txt users.txt > merged.txt
+cat merged.txt | sort | uniq -u > peeps_to_remove.txt
 
 cat peeps_to_remove.txt
 
