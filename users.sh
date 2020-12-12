4718e24f24089c9a460b08f2fd9fdb74a3997bf3
Saravanavasan <vasan@Ramadasss-MacBook-Air.local>
2020-12-12 08:02:10 -0800

    updated script

--- users.sh
+++ users.sh
@@ -2,14 +2,14 @@
 
 
 
-# eval getent passwd {$(awk '/^UID_MIN/ {print $2}' /etc/login.defs)..$(awk '/^UID_MAX/ {print $2}' /etc/login.defs)} | cut -d: -f1 >> users.txt
-ls /Users > users.txt
+eval getent passwd {$(awk '/^UID_MIN/ {print $2}' /etc/login.defs)..$(awk '/^UID_MAX/ {print $2}' /etc/login.defs)} | cut -d: -f1 >> users.txt
+
 cat authorized_users.txt users.txt > merged.txt
 cat merged.txt | sort | uniq -u > peeps_to_remove.txt
 
 cat peeps_to_remove.txt
 
-#cat peeps_to_remove.txt | while read line
-#do
-#    sudo userdel -r $line
-#done
+cat peeps_to_remove.txt | while read line
+do
+    sudo userdel -r $line
+done
